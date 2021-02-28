dcafbc130f66018de0ede686b25bca08e58586aa
Ryan Barkley <ryan@ryanbarkley.com>
2021-02-28 09:32:43 -0600

    Add yarn script for running dev with mocks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/globals.css";
+console.log(process.env.NEXT_PUBLIC_MSW_ENABLED);
 
-if (process.env.NODE_ENV === "development") {
+if (
+  process.env.NODE_ENV === "development" &&
+  process.env.NEXT_PUBLIC_MSW_ENABLED === "true"
+) {
   if (typeof window === "undefined") {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const { server } = require("../mocks/server");
